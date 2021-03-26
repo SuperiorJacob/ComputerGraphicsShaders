a77b57202e0a7402592f3d4b25d2d4e0b6905670
Jacob Cooper <s200503@students.aie.edu.au>
2021-03-26 12:04:59 +1100

    Adding jumping

--- ShaderGraph/Assets/Scripts/Player.cs
+++ ShaderGraph/Assets/Scripts/Player.cs
@@ -8,10 +8,16 @@ public class Player : MonoBehaviour
     CharacterController controller = null;
     Animator animator = null;
 
-    public float speed = 80.0f;
-    public float animationSpeed = 80.0f;
+    public float defaultSpeed = 5.0f;
     public float turnSpeed = 80.0f;
     public float pushPower = 2.0f;
+    public float jumpPower = 10.0f;
+    public float gravity = -9.81f;
+
+    private float speed = 5.0f;
+    private bool jumping = false; // FOr holding jump
+    private bool groundedPlayer = false;
+    private Vector3 playerVelocity = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -30,13 +36,38 @@ public class Player : MonoBehaviour
         float leftX = Input.GetAxis("Mouse X");
         float leftY = Input.GetAxis("Mouse Y");
 
-        //controller.SimpleMove(transform.forward * vertical * speed * Time.fixedDeltaTime);
-        controller.SimpleMove(((transform.right * horizontal) + (transform.forward * vertical)) * speed * Time.fixedDeltaTime);
+        speed = Input.GetKey(KeyCode.LeftShift) ? defaultSpeed * 1.5f : defaultSpeed;
 
-        transform.Rotate(transform.up, leftX * turnSpeed * Time.fixedDeltaTime);
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            jumping = false;
+            playerVelocity.y = 0f;
+        }
 
-        animator.SetFloat("Xpos", horizontal * animationSpeed * Time.fixedDeltaTime);
-        animator.SetFloat("Ypos", vertical * animationSpeed * Time.fixedDeltaTime);
+        transform.Rotate(transform.up, leftX * turnSpeed * Time.deltaTime);
+
+        animator.SetFloat("Xpos", Mathf.Clamp(horizontal, -1, 1), 1.0f, Time.deltaTime * 10.0f);
+        animator.SetFloat("Ypos", Mathf.Clamp(vertical, -1, 1), 1.0f, Time.deltaTime * 10.0f);
+
+        animator.SetBool("IsAiming", Input.GetMouseButton(1));
+
+        controller.Move((((transform.right * horizontal) + (transform.forward * vertical)) * speed) * Time.fixedDeltaTime);
+
+        animator.SetFloat("SpeedMod", speed/defaultSpeed);
+
+        bool jump = Input.GetButton("Jump") && groundedPlayer;
+        if (jump)
+        {
+            jumping = true;
+            playerVelocity.y += Mathf.Sqrt(jumpPower * -3.0f * gravity);
+        }
+
+        playerVelocity.y += gravity * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        if (jumping != animator.GetBool("IsJumping"))
+            animator.SetBool("IsJumping", jumping);
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit)
